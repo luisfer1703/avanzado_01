371cecf6f5b5e3e50b301c5851ecfb743e245419
luisfer1703 <camporico932@gmail.com>
2025-10-18 11:43:52 -0400

    proyecto de practica

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>mi primer proyecto</h1>
+      <p>hecho por luisfer</p>
+      </div>
+  )
+  }
+
+
+export default App
